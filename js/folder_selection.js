da38dab52656669bf4c0c471e225f08af8b97d47
Jake Marchewitz <7871951+jjmarchewitz@users.noreply.github.com>
2023-02-14 10:31:33 -0500

    REstructuring

---
+++ js/folder_selection.js
@@ -0,0 +1,6 @@
+import * as rust from '/pkg/index.js';
+
+// var myMod = require("../pkg");
+
+rust.rust_init()
+rust.greet("HEY")
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,24 @@
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-macro_rules! console_log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
-
+// This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
-pub fn rust_init() {
-    console_log!("START")
-}
+pub fn main_js() -> Result<(), JsValue> {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello! {}!", name));
+    // Your code goes here!
+    console::log_1(&JsValue::from_str("Hello world!"));
+
+    Ok(())
 }
